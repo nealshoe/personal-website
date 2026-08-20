89439e471db154d85b394ebfdb644370550b72e2
Neal Shoemaker <hello@nealshoe.com>
2026-08-19 21:59:25 -0400

    Initial static Flask site

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        app.static_folder,
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
+
+
+@app.route("/robots.txt")
+def robots():
+    return send_from_directory(
+        app.template_folder,
+        "robots.txt",
+        mimetype="text/plain",
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
